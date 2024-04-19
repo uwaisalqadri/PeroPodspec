9d57f8275206c67f4c423f65d77aa725eef0c7d8
Uwais Alqadri <uwais@nusantarabetastudio.com>
2024-04-19 14:37:16 +0800

    chore: upload framework

---
+++ Pero.framework/Headers/Pero.h
@@ -0,0 +1,19 @@
+//
+//  Pero.h
+//  Pero
+//
+//  Created by Uwais Alqadri on 30/3/24.
+//
+
+#import <Foundation/Foundation.h>
+#import <CommonCrypto/CommonCrypto.h>
+
+//! Project version number for Pero.
+FOUNDATION_EXPORT double PeroVersionNumber;
+
+//! Project version string for Pero.
+FOUNDATION_EXPORT const unsigned char PeroVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pero/PublicHeader.h>
+
+
